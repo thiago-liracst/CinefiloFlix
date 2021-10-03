2c5e9a577010c5a64fef93100c1faa198687d239
thiago-liracst <thiago.lira@ccc.ufcg.edu.br>
2021-10-03 09:30:06 -0300

    Initial User

---
+++ Logico/UserDB.pl
@@ -0,0 +1,19 @@
+:-use_module(library(csv)).
+:-include('Arquivos.pl').
+
+% Adiciona um usuario no arquivo
+addCliente(Nome, CPF, Idade, Sexo, GeneroFavorito, FilmeFavorito, SerieFavorita) :-
+    open('./dados/Users.csv', append, File),
+    writeln(File, (CPF, Nome, Idade, Sexo, GeneroFavorito, FilmeFavorito, SerieFavorita)),                 
+    close(File).
+
+% Verifica se o usuario existe, le o arquivo csv e chama o metodo verificaNaLista que vai ver se o usuario está na lista
+userExiste(Cpf, Result):-
+    lerCsvRowList('Users.csv', Users),
+    verificaNaLista(Cpf, Users, Result).
+
+% Retorna nome do Usuario a partir do cpf
+getNome(Cpf, Nome):-
+    lerCsvRowList('Users.csv', Users),
+    getEntidadeById(Cpf, Users, User),
+    elementByIndex(1, User, Nome).
